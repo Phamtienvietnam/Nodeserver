d8a535cdd5d060e26fa3ea1fd3607fd70a3cd148
Phamtienvietnam <88323288+Phamtienvietnam@users.noreply.github.com>
2023-05-06 12:14:18 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 443;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+	  console.log('Client connected - Tổng số Online => '+io.sockets.server.engine.clientsCount);
+	  console.log(io.sockets.server.engine.clientsCount);
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+	 console.log (msg);
+	 io.emit('message', msg);
+  });
+  socket.on('disconnect', () => console.log('Client disconnected - Online Còn lại '+io.sockets.server.engine.clientsCount));
+});
+
