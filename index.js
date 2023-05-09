4eaf02f5d81cb44a93707e63b0d52ccd7f24056d
Phamtienvietnam <88323288+Phamtienvietnam@users.noreply.github.com>
2023-05-09 10:04:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const app = express();
-
-const http = require('http');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const { instrument } = require("@socket.io/admin-ui");
 
-const server = http.createServer(app);
-var io = require("socket.io")(server, {
-cors: {
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
     origin: ["https://admin.socket.io"],
-    credentials: true,
-},
- });
+    credentials: true
+  }
+});
 
 instrument(io, {
- auth: false,
- mode: "development"
+  auth: false,
+  mode: "development",
 });
+
+httpServer.listen(3000);
