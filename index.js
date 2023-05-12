480f5f518f2101b49158fb29becc83d41a93bca1
Phamtienvietnam <88323288+Phamtienvietnam@users.noreply.github.com>
2023-05-12 07:23:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const socketIO = require('socket.io');
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 8080;
 const INDEX = '/index.html';
 
 const server = express()
