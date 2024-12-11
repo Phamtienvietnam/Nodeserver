321299b254e3b2593dd17a34f6a529bc425c3b7e
Phamtienvietnam <88323288+Phamtienvietnam@users.noreply.github.com>
2024-12-11 10:43:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-	  console.log('ID KET NOI '+io.sockets.id);
+	  console.log('ID KET NOI '+io.sockets.server.engine.clients.ID);
 	  console.log('Client connected - Tổng số Online => '+io.sockets.server.engine.clientsCount);
 	  console.log(io.sockets.server.engine.clientsCount);
   socket.on('chat message', msg => {
